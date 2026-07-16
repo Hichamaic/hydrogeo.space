ed8f18ac73108ae3b4bd87d839fae756a4dc7ae0
Hichamaic <aichouchmohamed@gmail.com>
2026-07-16 15:17:01 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "#0b1220",
+        color: "white",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>HydroGeo.Space</h1>
+    </main>
+  );
+}
